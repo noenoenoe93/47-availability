ed2e0a89662f7642d629600201d09076f293c4bb
noenoenoe93 <noelevanquang@gmail.com>
2022-03-16 17:58:50 +0100

    "fix / modif index.py"

--- index.py
+++ index.py
@@ -1,31 +1,34 @@
 import socket as sc
 import threading
 from colorama import Fore
+from mac_vendor_lookup import MacLookup
 
-ports = [135]
+# détection par ports
+ports = [135] # windows
+
 # partie scan
 def scan():
     for port in ports:
-        submit = ("10.0.0.17")
-        affichage = sc.gethostbyaddr(submit)
+        submit = ("192.168.0.29")
+        affichage = sc.gethostbyaddr(submit) # vérification avec le nom d'hôte si pc ne répond pas au ping
         print(f"démarrage du scan d'officience : {affichage}")
         sock = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
         result = sock.connect_ex((submit, port))
         sc.setdefaulttimeout(0)
-        hostname = []
-        hostname.append(hostname)
-        print(hostname)
-        '''
-        if result == 0 and ports[0]:
-            # print(Fore.GREEN + f"utlisateur détecté sur le port : {port}")
+        print(affichage)
+        windows_PC = ["DELL", "DESKTOP", "COMPUTER", "nono", "Intel Corporate"]
+        mac_PC = ["MAC"]
+
+        # vérification pour windows
+        if result == 0 and ports[0] or affichage[0] in windows_PC or MacLookup().lookup(submit) in windows_PC:
+            print(Fore.GREEN + f"utlisateur détecté sur le port : {port}")
             win = print("l'utilisateur utlise Windows")
             sock.close()
-            hostname.clear()
-            '''
-        if result == 0 and hostname in ("MAC"):
-                mac = print("l'utilisateur utilise Mac")
-                sock.close()
-                hostname.clear()
+
+            # vérification pour mac
+        if result == 0 or affichage[0] in mac_PC:
+            mac = print(Fore.GREEN + "l'utilisateur utilise Mac")
+            sock.close()
         else:
             print(Fore.RED + "utilisateur non détecté sur ce port")
             sock.close()
