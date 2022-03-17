5b0caee6535e3723d8691e1fba45679875c337ef
noenoenoe93 <noelevanquang@gmail.com>
2022-03-17 17:16:34 +0100

    "fix / modif index.py"

--- index.py
+++ index.py
@@ -1,32 +1,51 @@
 import socket as sc
+from sys import flags
 import threading
 from colorama import Fore
-from mac_vendor_lookup import MacLookup
+from scapy.all import *
+from scapy.layers.inet import *
 
+scan_TCP = sr1(IP(dst="10.0.0.107")/TCP(dport=[135], flags="S"))
+# scan_UDP = sr1(IP(dst="10.0.0.35")/UDP(dport=[135]))
+ttl_TCP = scan_TCP.getlayer(IP).ttl
+# ttl_UDP = scan_UDP.getlayer(IP).ttl
+print(ttl_TCP)
+# print(ttl_TCP, ttl_UDP)
+error = "système d'exploitation non détecté"
+
+if ttl_TCP == 128:
+    print(Fore.GREEN + "L'utilisateur utilise Windows")
+if ttl_TCP == 64:
+    print(Fore.GREEN + "L'utilisateur utilise Linux")
+if ttl_TCP == 64:
+    print(Fore.GREEN + "L'utilisateur utilise Mac")
+
+'''
 # détection par ports
 ports = [135] # windows
 
 # partie scan
 def scan():
     for port in ports:
-        submit = ("192.168.0.29")
+        submit = ("10.0.0.35")
         affichage = sc.gethostbyaddr(submit) # vérification avec le nom d'hôte si pc ne répond pas au ping
         print(f"démarrage du scan d'officience : {affichage}")
         sock = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
         result = sock.connect_ex((submit, port))
         sc.setdefaulttimeout(0)
         print(affichage)
-        windows_PC = ["DELL", "DESKTOP", "COMPUTER", "nono", "Intel Corporate"]
-        mac_PC = ["MAC"]
+        windows_OS = ["LAPTOP-"]
+        mac_OS = ["MACBOOKAIR-", "MacBook-"]
+        android_OS = ["Android.", "Android-"]
 
         # vérification pour windows
-        if result == 0 and ports[0] or affichage[0] in windows_PC or MacLookup().lookup(submit) in windows_PC:
+        if result == 0 and ports[0] or affichage[0] in windows_OS:
             print(Fore.GREEN + f"utlisateur détecté sur le port : {port}")
             win = print("l'utilisateur utlise Windows")
             sock.close()
 
             # vérification pour mac
-        if result == 0 or affichage[0] in mac_PC:
+        if result == 0 or affichage[0] in mac_OS:
             mac = print(Fore.GREEN + "l'utilisateur utilise Mac")
             sock.close()
         else:
@@ -38,6 +57,7 @@ for i in range(1):
     thread = threading.Thread(target=scan).start()
 
 
+'''
 '''
 # port[0] = "Windows"
 # print(port[0])
