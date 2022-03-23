d48b0b8a605fcc3fe6137668ceb9b3b620833923
noenoenoe93 <noelevanquang@gmail.com>
2022-03-23 16:53:01 +0100

    "modif/fix index.py"

--- index.py
+++ index.py
@@ -1,21 +1,24 @@
+from ast import pattern
+from gettext import find
 import socket as sc
 import threading
 from colorama import Fore
 from scapy.all import *
 from scapy.layers.inet import *
+import re
 
 # détection par ports
 ports = [135] # windows
 win = Fore.GREEN + "l'utilisateur utlise Windows"
-mac = Fore.GREEN + "l'utilisateur utilise Mac"
 linux = Fore.GREEN + "l'utilisateur utilise linux"
+mac = Fore.GREEN + "l'utilisateur utilise Mac"
 android = Fore.GREEN + "l'utilisateur utilise android"
 
 # partie scan
 def scan():
     for port in ports:
         # partie détection 1
-        submit = ("192.168.0.1")
+        submit = ("10.0.0.107")
         affichage = sc.gethostbyaddr(submit) # vérification avec le nom d'hôte si pc ne répond pas au ping
         print(f"démarrage du scan d'officience : {affichage}")
         sock = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
@@ -23,40 +26,49 @@ def scan():
         sc.setdefaulttimeout(0)
         print(affichage)
         windows_OS = ["LAPTOP-"]
-        mac_OS = ["MACBOOKAIR-", "MacBook-"]
+        mac_OS = ["MACBOOKAIR-", "MacBook-", "MACBOOKPRO-"]
         android_OS = ["Android.", "Android-"]
         linux_OS = ["LINUX"]
 
         # partie vérification windows
-        if result == 0 and ports[0] and affichage[0] in windows_OS:
-            print(Fore.GREEN + f"utlisateur détecté sur le port : {port}")
+        if result == 0:
+            OS_WIN_MATCH = list(filter(lambda i: "ALOPT-" in i, windows_OS))
+            print(OS_WIN_MATCH)
             print(win)
-            sock.close()
-
-        # vérification pour mac
-        if result == 0 and affichage[0] in mac_OS:
-            print(mac)
-            sock.close()
-
-        # vérification pour linux
-        if result == 0 and affichage[0] in linux_OS:
-            print(linux)
-            sock.close()
-
-        # vérification pour android
-        if result == 0 and affichage[0] in android_OS:
-            print(android)
+            print(Fore.GREEN + f"utlisateur détecté sur le port : {port}")
             sock.close()
         
+        '''
+        # vérification pour mac
+        if result == 0:
+            OS_MAC_MATCH = [i for i in mac_OS if i.__contains__("ABCMOKIRPabcmokirp0123456789-")]
+            print(OS_MAC_MATCH)
+            print(mac)
+            sock.close()
+        '''
+        '''
+        # vérification pour linux
+        if result == 0 and affichage[0] in re.search(linux_OS):
+            print(linux)
+            sock.close()
+        '''
+        '''
+        # vérification pour android
+        if result == 0 and affichage[0] in re.search(android_OS):
+            print(android)
+            sock.close()
+
         elif result == 1:
             sock.close()
-            scan2()
-
+            exit()
+        '''
+        
+        '''
 def scan2(): 
     # partie détection 2 si 1 ne marche pas
-    scan_TCP = sr1(IP(dst="192.168.0.1")/TCP(dport=[135], flags="S"))
-    scan_UDP = sr1(IP(dst="192.168.0.1")/UDP(dport=[67]))
-    scan_ICMP = sr1(IP(dst="192.168.0.1")/ICMP())
+    scan_TCP = sr1(IP(dst="10.0.0.35")/TCP(dport=[135], flags="S"))
+    scan_UDP = sr1(IP(dst="10.0.0.35")/UDP(dport=[67]))
+    scan_ICMP = sr1(IP(dst="10.0.0.35")/ICMP())
     ttl_TCP = scan_TCP.getlayer(IP).ttl
     ttl_UDP = scan_UDP.getlayer(IP).ttl
     ttl_ICMP = scan_ICMP.getlayer(IP).ttl
@@ -67,11 +79,12 @@ def scan2():
 
     if ttl_TCP == 64 or ttl_UDP == 64 or ttl_ICMP == 64:
         print(mac)
-
+        '''
 # partie accélération de scan
 for i in range(1):
     thread = threading.Thread(target=scan).start()
-    thread = threading.Thread(target=scan2).start()
+    # thread = threading.Thread(target=scan2).start()
+
 
 '''
 # port[0] = "Windows"
